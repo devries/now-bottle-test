cee41b59cfe0f5f6d04d95a4a6a1a67eb780371c
Christopher De Vries <devries@idolstarastronomer.com>
2019-04-04 16:31:09 -0400

    Added a little HTML template

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import bottle
+import os
 
 app = bottle.app()
 
 @bottle.route('/')
 def root_index():
-    return "Hello World!"
+    region = os.environ.get('NOW_REGION', 'unknown')
+    return bottle.template('index', region = region)
 
 if __name__=='__main__':
     bottle.debug(True)
-    bottle.run(app=app, host='localhost', port=8080)
+    bottle.run(app=app, host='localhost', port=5000)
---
+++ views/index.tpl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Hello World</title>
+</head>
+<body>
+<h1>Hello World!</h1>
+<h2>From Zeit Now!</h2>
+<p>This service is running in the {{region}} region.</p>
+</body>
+</html>
